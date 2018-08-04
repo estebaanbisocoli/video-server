eeebccb1e120f183511d535f6c88c893d8f008f9
ESTEBAN <estebaanbisocoli@gmail.com>
2018-08-04 20:26:40 -0300

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <video-player></video-player>
+</template>
+
+<script>
+import VideoPlayer from "./components/VideoPlayer.vue";
+export default {
+  name: "app",
+  components: {
+    VideoPlayer
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/VideoPlayer.vue
@@ -0,0 +1,12 @@
+<template>
+    <video id="videoPlayer" controls>
+        <source src="http://localhost:3000/video" type="video/mp4">
+    </video>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
